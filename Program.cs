7396883e8b1df35a5a95b938a4f2e017bb2611ad
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-11-07 15:47:41 -0600

    keyboard class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Keyboard 
+//Incoming letter to code/ outgoing code back to letter
+public class Keyboard
+{
+    public string Alphabet { get; }
+    public Keyboard()
+    {
+        Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    public int EncodeLetter(char letter)
+    {
+        letter = char.ToUpper(letter);
+
+        if (Alphabet.Contains(letter))
+        {
+            return Alphabet.IndexOf(letter);
+        }
+        else
+        {
+            throw new ArgumentException("The input is not a valid keyboard letter");
+        }
+    }
+    public char DecodeKey(int key)
+    {
+        return Alphabet[key];
+    }
+}
+
+//Rotor Class
+
+//Plugboard Class
